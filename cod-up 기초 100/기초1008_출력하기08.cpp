d1529491069ec870c78bff2c328f2f249370fde3
김수빈 <52395282+kimsubin-26@users.noreply.github.com>
2020-07-22 16:25:37 +0900

    Create 기초1008_출력하기08.cpp

---
+++ cod-up 기초 100/기초1008_출력하기08.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	printf("\u250C\u252C\u2510\n");
+	printf("\u251C\u253C\u2524\n");
+	printf("\u2514\u2534\u2518\n");
+	return 0;
+}
+
+//유니코드 이용
