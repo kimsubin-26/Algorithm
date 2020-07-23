507f64b780cce84bc5d985548620da57e67437a1
김수빈 <52395282+kimsubin-26@users.noreply.github.com>
2020-07-23 19:27:58 +0900

    정수 입력받아 아스키 문자로 출력하기

---
+++ cod-up 기초 100/기초_출력변환_1037.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	
+	int a;
+	scanf("%d", &a);
+	printf("%c", a);
+	
+	//%c문자이고 아스키코드 변환시 사용 비슷한 %s 는 문자열 다른점은 c는 문자이고 s는 문자열이라 이경우는 문자로 출력하는거라 c를 쓰는게 좋다고 판단했다.
+
+	
+}
