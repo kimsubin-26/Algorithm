5547d176c48e4ef5f71f8874b8a01f1608071a40
김수빈 <52395282+kimsubin-26@users.noreply.github.com>
2020-07-23 19:17:19 +0900

    10진 정수 입력받아 16진수로 출력하기1

---
+++ cod-up 기초 100/기초_출력변환_1032.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	
+	int a;
+	scanf("%d", &a);
+	printf("%x", a);
+	//16진수 출력은 "%x"이다.
+	//16진수는 한자리에 16개(0 1 2 3 4 5 6 7 8 9 a b c d e f)를 사용한다
+	//16진수의 a는 10진수의 10, b는 11.... 과 같다
+	
+
+	
+}
