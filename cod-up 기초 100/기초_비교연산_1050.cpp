b1e2051086d770dfe3f6f8d044d8f7969682ef96
김수빈 <52395282+kimsubin-26@users.noreply.github.com>
2020-07-25 19:22:48 +0900

    두 정수 입력받아 비교하기2

---
+++ cod-up 기초 100/기초_비교연산_1050.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	
+	/*
+	조건
+	두 정수 a, b가 공백을 두고 입력된다.
+	- 2147483648 <= a, b <= +2147483647
+	
+	*/
+	long long a, b;
+	cin >> a >> b;
+	if (a == b) {
+		cout << 1;
+	}
+	else {
+		cout << 0;
+	}
+	return 0;
+}
