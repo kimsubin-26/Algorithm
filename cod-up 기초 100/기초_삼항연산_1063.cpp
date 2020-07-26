22b96554d490e09787a5441ee2720447ab2a546a
김수빈 <52395282+kimsubin-26@users.noreply.github.com>
2020-07-26 20:30:42 +0900

    두 정수 입력받아 큰 수 출력하기

---
+++ cod-up 기초 100/기초_삼항연산_1063.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	
+	
+	int a,b;
+	cin >> a>>b;
+	cout << (a >b ? a:b); // 3개의 요소로 이루어지는 3항(ternary) 연산자는 "조건식 ? (참일 때의 값) : (거짓일 때의 값)” 의 형태로 사용하는 연산자이다.
+}
