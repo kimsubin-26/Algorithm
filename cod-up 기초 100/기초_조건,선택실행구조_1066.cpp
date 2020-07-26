402a26322d88ce73fb18fc6f2b59389b7bd43083
김수빈 <52395282+kimsubin-26@users.noreply.github.com>
2020-07-26 20:39:17 +0900

    정수 3개 입력받아 짝/홀 출력하기

---
+++ cod-up 기초 100/기초_조건,선택실행구조_1066.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	
+	//세 정수 a, b, c가 입력되었을 때, 짝(even)/홀(odd)을 출력해보자.
+	int a,b,c;
+	cin >> a>>b>>c;
+	if (a % 2 == 0) {
+		cout << "even" << endl;
+	}
+	else {
+		cout << "odd" << endl;
+	}
+	if (b % 2 == 0) {
+		cout << "even" << endl;
+	}
+	else {
+		cout << "odd" << endl;
+	}
+	if (c % 2 == 0) {
+		cout << "even" << endl;
+	}
+	else {
+		cout << "odd" << endl;
+	}
+}
