c6a800df3b152a6c4472bca1ac1149fec1c1e53c
김수빈 <52395282+kimsubin-26@users.noreply.github.com>
2020-07-28 13:09:28 +0900

    원하는 문자가 입력될 때까지 반복 출력하기

---
+++ cod-up 기초 100/기초_종합_1079.cpp
@@ -0,0 +1,16 @@
+#include<iostream>>
+using namespace std;
+
+int main() {
+	char a;
+	while (true) {
+		cin >> a;
+		if (a == 'q') {
+			cout << a << endl;
+			break;
+		}
+		else {
+			cout << a << endl;
+		}
+	}
+}
