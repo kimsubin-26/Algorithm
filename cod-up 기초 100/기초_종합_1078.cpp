b80b5daec73136faccff7288ed49b06865907c4e
김수빈 <52395282+kimsubin-26@users.noreply.github.com>
2020-07-28 13:05:45 +0900

    짝수 합 구하기

---
+++ cod-up 기초 100/기초_종합_1078.cpp
@@ -0,0 +1,16 @@
+#include<iostream>>
+using namespace std;
+
+int main() {
+	int n;
+	int sum;
+	cin >> n>>sum;
+	for (int i = 1; i <= n; i++) {
+		if (i % 2 == 0) {
+			sum = sum + i;
+		}
+	}
+
+	cout << sum << endl;
+	
+}
