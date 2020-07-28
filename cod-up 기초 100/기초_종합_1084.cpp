e9a2b33324ccccb7d4f1cb2ba3d0b5262199ae09
김수빈 <52395282+kimsubin-26@users.noreply.github.com>
2020-07-28 14:17:27 +0900

    빛 섞어 색 만들기

---
+++ cod-up 기초 100/기초_종합_1084.cpp
@@ -0,0 +1,17 @@
+#include<iostream>>
+using namespace std;
+
+int main() {
+	int r, g, b;
+	int c=0;
+	cin >> r >> g >> b;
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < g; j++) {
+			for (int k = 0; k < b; k++) {
+				printf("%d %d %d\n", i, j, k);
+				c += 1;
+			}
+		}
+	}
+	cout << c;
+}
