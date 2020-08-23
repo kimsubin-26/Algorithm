a2c445a6440c950917c6ff5082cd7a233cff0022
김수빈 <52395282+kimsubin-26@users.noreply.github.com>
2020-08-23 13:19:21 +0900

    백준 10171번 고양이 c++ (고양이는 귀엽다 우주최강)

---
+++ Baekjoon/쉬운게풀고싶을때/10171.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+//c++ \출력하려면 \\두번써주면된다
+
+int main() {
+	cout << "\\    /\\" << endl;;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  ) " << endl;
+	cout << " \\(__)| " << endl;
+	return 0;
+}
