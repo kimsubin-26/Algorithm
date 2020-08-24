7c8471e7215d547d3095e97c23116973c84d9839
김수빈 <52395282+kimsubin-26@users.noreply.github.com>
2020-08-24 10:40:27 +0900

    백준 10172번 개 c++ dogcute

---
+++ Baekjoon/쉬운게풀고싶을때/10172.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+//따옴표 주의 따옴표를 \"라고 써써 출력해야 문자그대로 출력가능합니다
+int main() {
+	cout << "|\\_/|" << endl;;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+	return 0;
+}
